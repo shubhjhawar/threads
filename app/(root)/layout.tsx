30bb9937c39100ffd7cf7512250bc04869fcea90
Shubh <shubhjhawar78@gmail.com>
2023-08-09 14:44:38 -0400

    homepage layout implemented

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,10 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import "../globals.css"
+import TopBar from "@/components/shared/TopBar";
+import LeftSideBar from "@/components/shared/LeftSideBar";
+import RightSideBar from "@/components/shared/RightSideBar";
+import BottomBar from "@/components/shared/BottomBar";
 
 export const metadata = {
     title:"Threads",
@@ -16,7 +20,19 @@ export default function RootLayout({ children,
     return (
         <ClerkProvider>
             <html lang="en">
-                <body className={inter.className}>{children}</body>
+                <body className={inter.className}>
+                    <TopBar />
+                    <main className='flex flex-row'>
+                        <LeftSideBar />
+                        <section className="main-container">
+                            <div className="w-full max-w-4xl">
+                                {children}
+                            </div>
+                        </section>
+                        <RightSideBar />
+                    </main>
+                    <BottomBar />
+                </body>
             </html>
         </ClerkProvider>
     )
