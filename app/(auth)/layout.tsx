77985c61b495bcaf937aa85d8d0205fc8cdfb48e
Shubh <shubhjhawar78@gmail.com>
2023-08-09 12:27:12 -0400

    auth implemented successfully

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -9,15 +9,14 @@ export const metadata = {
 
 const inter = Inter({subsets: ["latin"]})
 
-export default function RootLayout(
-    { children }: {children : React.ReactNode}
-    ) {
+export default function RootLayout({ children, 
+}: {
+    children : React.ReactNode
+}) {
     return (
         <ClerkProvider>
             <html lang="en">
-                <body className={`${inter.className} bg-dark-1`}>
-                    {children}
-                </body>
+                <body className={`${inter.className} bg-dark-1`}>{children}</body>
             </html>
         </ClerkProvider>
     )
